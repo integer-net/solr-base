b31dd84a03935ce85e3812b18da21ac53230b44d
Andreas von Studnitz <avs@integer-net.de>
2017-04-10 15:20:19 +0200

    Move interface declaration from registration.php to PHP file which can be loaded via Autoloader

---
+++ src/Solr/Implementor/SolrRequestFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_Solr
+ * @copyright  Copyright (c) 2017 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */
+
+namespace IntegerNet\Solr\Implementor
+{
+    /** @deprecated this is an alias for SolrRequestFactory due to limitations of the Magento 2 object manager */
+    interface SolrRequestFactoryInterface extends SolrRequestFactory {}
+}
